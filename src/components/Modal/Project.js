8c0d7e8d9f0fc2c88840e700df441fc22d59ff86
zachjbrowning <zachjbrowning@outlook.com>
2020-09-29 10:20:59 +1000

    parser added

--- src/components/Modal/Project.js
+++ src/components/Modal/Project.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import styles from './Modal.module.scss';
 
+import parse from '../../../lib/utils/parser';
+
 export default function Project(props) {
+    console.log(props.item?.learning)
     return <div className={styles.content}>
         <h2 className={styles.header}>{props.item?.title}.</h2>
         <p>{props.item?.date}</p>
 
         <h3 className={styles.subheader}>Summary</h3>
-        <p>{props.item?.summary}</p>
+        <p>{props.item?.summary ? parse(props.item?.summary) : ""}</p>
 
         <h3 className={styles.subheader}>Learning outcomes</h3>
-        <p>{props.item?.summary}</p>
+        <ul>{props.item?.learning ? props.item.learning.map((out, index) => <li key={index}>{parse(out)}</li>) : ""}</ul>
 
         <h3 className={styles.subheader}>Notes</h3>
-        <p>{props.item?.notes}</p>
+        <ul>{props.item?.notes ? props.item.notes.map((out, index) => <li key={index}>{parse(out)}</li>) : ""}</ul>
         
     </div>
 }
