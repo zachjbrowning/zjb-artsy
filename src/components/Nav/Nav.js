8137ff128b6783514cfa065f2b8cd94d5c5f3fbc
zachjbrowning <zachjbrowning@outlook.com>
2020-10-01 20:09:13 +1000

    reactive nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 
 import styles from './Nav.module.scss';
 
+
+import { close } from '../Utils/Icon';
+
+
 export default function Nav() {
     const history = useHistory();
+    const [isOpen, setOpen] = useState(false);
+
+    var word = "home";
+    switch(history.location.pathname) {
+        case '/projects':
+            word = "projects";
+            break;
+        case '/recipes':
+            word = 'recipes';
+            break;
+        case '/contact':
+            word = 'contact';
+            break;
+    }
     
     function setActive(e) {
         e.target.classList.add(styles.active);
+        setOpen(false);
         switch (history.location.pathname) {
             case '/':
-                document.getElementById("nav-about").classList.remove(styles.active);
                 break;
             case '/projects':
                 document.getElementById("nav-projects").classList.remove(styles.active);
@@ -26,10 +44,11 @@ export default function Nav() {
     }
     
     return <div id="nav" className={styles.constrict}>
-        <div className={styles.nav}>
-            <Link id="nav-about" onClick={setActive} to={"/"} className={`${styles.navItem} ${history.location.pathname === '/' ? styles.active : ""}`}>
-                about
-            </Link>
+       
+        <div className={`${styles.nav} ${isOpen ? styles.open : ""}`}>
+            <div onClick={() => setOpen(false)} className={styles.close}>
+                <svg viewBox="0 0 8192 8192">{close}</svg>
+            </div>
             <Link id="nav-projects" onClick={setActive} to={"/projects"} className={`${styles.navItem} ${history.location.pathname === '/projects' ? styles.active : ""}`}>
                 projects
             </Link>
@@ -40,5 +59,15 @@ export default function Nav() {
                 contact
             </Link>
         </div>
+        
+        <div className={styles.mini}>
+            <p>{word}</p>
+            <div onClick={() => setOpen(true)} className={styles.burgerbox} >
+                <svg width='30' height='30' viewBox='0 0 30 30'>
+                    <path strokeMiterlimit='10' strokeWidth='2' d='M4 7h22M4 15h22M4 23h22'/>
+                </svg>
+            </div>
+        </div>
+        
     </div>
 }
